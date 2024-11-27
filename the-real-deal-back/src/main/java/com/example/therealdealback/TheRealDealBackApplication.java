96d352eaaecf7b0c051f9cca96485f863135f3de
MatheoZimmerCESI <matheo.zimmer@viacesi.fr>
2024-11-27 11:50:21 +0100

    [ZMR] - Initialisation du back java - SpringBoot

---
+++ the-real-deal-back/src/main/java/com/example/therealdealback/TheRealDealBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.therealdealback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRealDealBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheRealDealBackApplication.class, args);
+    }
+
+}
---
+++ the-real-deal-back/src/test/java/com/example/therealdealback/TheRealDealBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.therealdealback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheRealDealBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
